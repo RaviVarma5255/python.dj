8ff3fe04cde73bd741bec98183eae45da94fdb12
D S S RAVI VARMA <ravivarma52550@gmail.com>
2025-11-08 11:39:35 +0530

    program

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/',home)
 ]
