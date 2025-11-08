e6caaa567e477ce2cd381f7852fd7d5433532e0c
D S S RAVI VARMA <ravivarma52550@gmail.com>
2025-11-08 12:07:00 +0530

    addition

--- myproject/basic/views.py
+++ myproject/basic/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("hello world !")
-# Create your views here.
+
+def add(request):
+     a = 20
+     b = 30
+     c= a+b
+     return HttpResponse(f"the sum of {a} and {b} is {c}")
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from basic.views import home
+from basic.views import add
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first/',home)
+    path('first/',home),
+    path('second/',add)
 ]
